50f6b3483661a4dd50a70ecd790c44d650f712cc
Ghabasjr <khalidumar089@gmail.com>
2024-12-06 12:25:44 +0100

    making it better

--- src/Components/DashBoard/DashBoard.tsx
+++ src/Components/DashBoard/DashBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Button, Image, Input, Paragraph, Text, Flex } from "theme-ui";
 import HorizontalLine from "../HorizontalLine/HorizontalLine";
